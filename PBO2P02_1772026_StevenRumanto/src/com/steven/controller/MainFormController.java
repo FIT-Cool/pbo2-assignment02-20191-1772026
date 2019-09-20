8d733195bc4062dcf6b4e3597575b28305eac6b7
1772026 <35952854+1772026@users.noreply.github.com>
2019-09-20 11:35:24 +0700

    update tag name

--- PBO2P02_1772026_StevenRumanto/src/com/steven/controller/MainFormController.java
+++ PBO2P02_1772026_StevenRumanto/src/com/steven/controller/MainFormController.java
@@ -16,7 +16,10 @@ import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
-
+/**
+ * Steven Rumanto
+ * 1772026
+ */
 public class MainFormController implements Initializable {
     List<String> myCategory = new ArrayList<>();
     @FXML
